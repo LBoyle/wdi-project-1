e22e31a4beca60a6c51dbc991048b0d161e62820
LBoyle <louis_boyle@hotmail.com>
2017-04-03 17:54:02 +0100

    hooked up the computer player again, found a bug in the refactored code

--- js/app.js
+++ js/app.js
@@ -46,12 +46,9 @@ $(() => {
   //   }
   // }
 
-  // createBoard just makes and appends all the DOM elements, it looks complicated, but it's not
-  // it only runs once, once the click event is setup, we don't need to change anything, the clickHandler calls the other functions.
-
   function createBoard() {
     console.log('Initialized');
-    // gameMode = prompt('Which mode? "PvP", "PvC" or "CvC"?').toLowerCase();
+    gameMode = prompt('Which mode? "PvP", "PvC" or "CvC"?').toLowerCase();
     const $body = $('body');
     const $header = $(document.createElement('h1'));
     const $instr = $(document.createElement('p'));
@@ -95,16 +92,17 @@ $(() => {
       }
     }
     $body.append($header, $instr, $main, $scoreLeft, $scoreRight);
-    // if (gameMode === 'pvp') {
-    $('.box').on('click', PvP.bind(this));
-    // }
-    // if (gameMode === 'pvc') {
-    //   // $('.box').on('click', PvC.bind(this));
-    //   PvCTimer();
-    // }
-    // if (gameMode === 'cvc') {
-    //   CvCTimer();
-    // }
+    if (gameMode === 'pvp') {
+      // $('.box').on('click', PvP.bind(this));
+      PvPController();
+    }
+    if (gameMode === 'pvc') {
+      // $('.box').on('click', PvC.bind(this));
+      PvCTimer();
+    }
+    if (gameMode === 'cvc') {
+      CvCTimer();
+    }
     legal = false;
   }
 
@@ -139,21 +137,21 @@ $(() => {
     }
   }
 
-  // function PvPController() { // not a timer
-  //   if (count === 0 || count % 2 === 0) {
-  //     $('.box').on('click', function(e) {
-  //       PvC(e, 'B', 'W');
-  //       $('.box').off();
-  //       PvCTimer();
-  //     });
-  //   } else {
-  //     $('.box').on('click', function(e) {
-  //       PvC(e, 'W', 'B');
-  //       $('.box').off();
-  //       PvCTimer();
-  //     });
-  //   }
-  // }
+  function PvPController() { // not a timer
+    if (count === 0 || count % 2 === 0) {
+      $('.box').on('click', function(e) {
+        PvC(e, 'B', 'W');
+        $('.box').off();
+        PvCTimer();
+      });
+    } else {
+      $('.box').on('click', function(e) {
+        PvC(e, 'W', 'B');
+        $('.box').off();
+        PvCTimer();
+      });
+    }
+  }
 
   // CvC
 
@@ -188,99 +186,26 @@ $(() => {
     if (!$(e.target).hasClass('clicked')) {
       let row = e.target.id.split('')[0];
       let col = parseInt(e.target.id.split('')[1]);
-      // console.log($('#'+row+col)[0]);
       count++;
-      // if (count === 0 || count % 2 === 0) {
-        // const cpResponse = CP.computerPlay('W', boardModel, keys);
-        // row = cpResponse.split('')[1];
-        // col = parseInt(cpResponse.split('')[2]);
-        // isLegal(row, col, 'W');
-        // if (legal === true) {
-        //   boardModel[row][col] = 'W';
-        //   doFlip('B','W', chipsToFlip);
-        //   whiteTiles = numTiles('W');
-        //   blackTiles = numTiles('B');
-        //   // $(e.target).removeClass('N');
-        //   $(cpResponse).removeClass('N');
-        //   // $(e.target).addClass('W clicked');
-        //   $(cpResponse).addClass('W clicked');
-        //   legal = false;
-        //   $('.left').text('Black, Your turn');
-        //   $('.right').text('White team');
-        //   $counterR.text('Tiles: '+whiteTiles+' Taken: '+blackTaken);
-        //   $counterL.text('Tiles: '+blackTiles+' Taken: '+whiteTaken);
-        // } else {
-        //   count--;
-        // }
-      // } else {
-        isLegal(row, col, 'B');
-        if (legal === true) {
-          boardModel[row][col] = 'B';
-          doFlip('W','B', chipsToFlip);
-          blackTiles = numTiles('B');
-          whiteTiles = numTiles('W');
-          $('#'+row+col).removeClass('N');
-          $('#'+row+col).addClass('B clicked');
-          legal = false;
-          $('.right').text('White, Your turn');
-          $('.left').text('Black team');
-          $counterL.text('Tiles: '+blackTiles+' Taken: '+whiteTaken);
-          $counterR.text('Tiles: '+whiteTiles+' Taken: '+blackTaken);
-        } else {
-          // if legal is false, nothing has been pushed to chipsToFlip
-          // so just deincrement turn counter
-          count--;
-        }
+      isLegal(row, col, 'B');
+      if (legal === true) {
+        boardModel[row][col] = 'B';
+        doFlip('W','B', chipsToFlip);
+        blackTiles = numTiles('B');
+        whiteTiles = numTiles('W');
+        $('#'+row+col).removeClass('N');
+        $('#'+row+col).addClass('B clicked');
+        legal = false;
+        $('.right').text('White, Your turn');
+        $('.left').text('Black team');
+        $counterL.text('Tiles: '+blackTiles+' Taken: '+whiteTaken);
+        $counterR.text('Tiles: '+whiteTiles+' Taken: '+blackTaken);
+      } else {
+        count--;
+      }
       // }
     } // hasClass 'clicked'
   } // end of PvC function
-
-  // PvP function works fine
-
-  function PvP(e) {
-    if (!$(e.target).hasClass('clicked')) {
-      const row = e.target.id.split('')[0];
-      const col = parseInt(e.target.id.split('')[1]);
-      count++;
-      if (count === 0 || count % 2 === 0) {
-        isLegal(row, col, 'W');
-        if (legal === true) {
-          boardModel[row][col] = 'W';
-          doFlip('B','W', chipsToFlip);
-          whiteTiles = numTiles('W');
-          blackTiles = numTiles('B');
-          $('#'+row+col).removeClass('N');
-          $('#'+row+col).addClass('W clicked');
-          legal = false;
-          $('.left').text('Black, Your turn');
-          $('.right').text('White team');
-          $counterR.text('Tiles: '+whiteTiles+' Taken: '+blackTaken);
-          $counterL.text('Tiles: '+blackTiles+' Taken: '+whiteTaken);
-        } else {
-          count--;
-        }
-      } else {
-        isLegal(row, col, 'B');
-        if (legal === true) {
-          boardModel[row][col] = 'B';
-          doFlip('W','B', chipsToFlip);
-          blackTiles = numTiles('B');
-          whiteTiles = numTiles('W');
-          $('#'+row+col).removeClass('N');
-          $('#'+row+col).addClass('B clicked');
-          legal = false;
-          $('.right').text('White, Your turn');
-          $('.left').text('Black team');
-          $counterL.text('Tiles: '+blackTiles+' Taken: '+whiteTaken);
-          $counterR.text('Tiles: '+whiteTiles+' Taken: '+blackTaken);
-        } else {
-          // if legal is false, nothing has been pushed to chipsToFlip
-          // so just deincrement turn counter
-          count--;
-        }
-      }
-    } // hasClass 'clicked'
-  } // end of PvP function
 
 // Here is the new much shorter isLegal function
 // it does the same thing that my three long ass functions did
@@ -349,88 +274,6 @@ $(() => {
     });
   }
 
-  // function isLegal2(row, col, current) {
-  //   const prevRowData = [];
-  //   const nextRowData = [];
-  //   const prevColData = [];
-  //   const nextColData = [];
-  //   const tlTobrPrevData = [];
-  //   const tlTobrNextData = [];
-  //   const blTotrPrevData = [];
-  //   const blTotrNextData = [];
-  //   // 7 because I don't add the square you just clicked
-  //   for (let i=0; i<7; i++) {
-  //     // for the Row, trying to read from a Dict with
-  //     // dict[foo][outside range or undefined] returns undefined
-  //     prevRowData.push(boardModel[row][col-(i+1)]);
-  //     nextRowData.push(boardModel[row][col+(i+1)]);
-  //     // for Cols though, trying to read from a dict with
-  //     // dict[undefined][number or undefined]
-  //     // produces an error so I had to check each one individually
-  //     prevColData.push((boardModel[keys[keys.indexOf(row)-(i+1)]] !== undefined) ? boardModel[keys[keys.indexOf(row)-(i+1)]][col] : undefined);
-  //     nextColData.push((boardModel[keys[keys.indexOf(row)+(i+1)]] !== undefined) ? boardModel[keys[keys.indexOf(row)+(i+1)]][col] : undefined);
-  //     // this gets confusing
-  //     // can you believe it works though!?
-  //     tlTobrPrevData.push((boardModel[keys[keys.indexOf(row)-(i+1)]] !== undefined) ? boardModel[keys[keys.indexOf(row)-(i+1)]][col-(i+1)] : undefined);
-  //     tlTobrNextData.push((boardModel[keys[keys.indexOf(row)+(i+1)]] !== undefined) ? boardModel[keys[keys.indexOf(row)+(i+1)]][col+(i+1)] : undefined);
-  //     blTotrPrevData.push((boardModel[keys[keys.indexOf(row)+(i+1)]] !== undefined) ? boardModel[keys[keys.indexOf(row)+(i+1)]][col-(i+1)] : undefined);
-  //     blTotrNextData.push((boardModel[keys[keys.indexOf(row)-(i+1)]] !== undefined) ? boardModel[keys[keys.indexOf(row)-(i+1)]][col+(i+1)] : undefined);
-  //   }
-  //   // changed to push all legal blocks to chipsToFilp to be flipped
-  //   // this function returns nothing
-  //   // I can now use the same one function to check all directions
-  //   checkBoard(row, col, current, prevRowData, nextRowData, 'h');
-  //   checkBoard(row, col, current, prevColData, nextColData, 'v');
-  //   checkBoard(row, col, current, tlTobrPrevData, tlTobrNextData, 'd1');
-  //   checkBoard(row, col, current, blTotrPrevData, blTotrNextData, 'd2');
-  // }
-
-  // function pushChips(plane, num, dir, row, col, player) {
-  //   // num is number of blocks to be taken
-  //   for (let i=0; i<num; i++) {
-  //     if (plane === 'h') {
-  //       if (dir === 'neg') {
-  //         chipsToFlip.push('#'+row+(col-(i+1)).toString());
-  //         boardModel[row][col-(i+1)] = player;
-  //       }
-  //       if (dir === 'pos') {
-  //         chipsToFlip.push('#'+row+(col+(i+1)).toString());
-  //         boardModel[row][col+(i+1)] = player;
-  //       }
-  //     }
-  //     if (plane === 'v') {
-  //       if (dir === 'neg') {
-  //         chipsToFlip.push('#'+(keys[keys.indexOf(row)-(i+1)])+(col).toString());
-  //         boardModel[keys[keys.indexOf(row)-(i+1)]][col] = player;
-  //       }
-  //       if (dir === 'pos') {
-  //         chipsToFlip.push('#'+(keys[keys.indexOf(row)+(i+1)])+(col).toString());
-  //         boardModel[keys[keys.indexOf(row)+(i+1)]][col] = player;
-  //       }
-  //     }
-  //     if (plane === 'd1') {
-  //       if (dir === 'neg') {
-  //         chipsToFlip.push('#'+(keys[keys.indexOf(row)-(i+1)])+(col-(i+1)).toString());
-  //         boardModel[keys[keys.indexOf(row)-(i+1)]][col-(i+1)] = player;
-  //       }
-  //       if (dir === 'pos') {
-  //         chipsToFlip.push('#'+(keys[keys.indexOf(row)+(i+1)])+(col+(i+1)).toString());
-  //         boardModel[keys[keys.indexOf(row)+(i+1)]][col+(i+1)] = player;
-  //       }
-  //     }
-  //     if (plane === 'd2') {
-  //       if (dir === 'neg') {
-  //         chipsToFlip.push('#'+(keys[keys.indexOf(row)+(i+1)])+(col-(i+1)).toString());
-  //         boardModel[keys[keys.indexOf(row)+(i+1)]][col-(i+1)] = player;
-  //       }
-  //       if (dir === 'pos') {
-  //         chipsToFlip.push('#'+(keys[keys.indexOf(row)-(i+1)])+(col+(i+1)).toString());
-  //         boardModel[keys[keys.indexOf(row)-(i+1)]][col+(i+1)] = player;
-  //       }
-  //     }
-  //   } // end of loop
-  // } // end of pushChips function
-
   function numTiles(player) {
     let num = 0;
     for (var i=0; i<keys.length; i++) {
@@ -460,64 +303,5 @@ $(() => {
     }
   } // end of doFlip function
 
-  // I've rigged this function to do horizontal, vertical or diagonal checks based on input
-  // function checkBoard(row, col, player, prev, next, plane) {
-  //   let enemy = '';
-  //   if (player === 'B') {
-  //     enemy = 'W';
-  //   }else {
-  //     enemy = 'B';
-  //   }
-  //   if (next[0] === enemy && next[1] === player) {
-  //     legal = true;
-  //     pushChips(plane, 1, 'pos', row, col, player);
-  //   }
-  //   if (prev[0] === enemy && prev[1] === player) {
-  //     legal = true;
-  //     pushChips(plane, 1, 'neg', row, col, player);
-  //   }
-  //   if (next[0] === enemy && next[1] === enemy && next[2] === player) {
-  //     legal = true;
-  //     pushChips(plane, 2, 'pos', row, col, player);
-  //   }
-  //   if (prev[0] === enemy && prev[1] === enemy && prev[2] === player) {
-  //     legal = true;
-  //     pushChips(plane, 2, 'neg', row, col, player);
-  //   }
-  //   if (next[0] === enemy && next[1] === enemy && next[2] === enemy && next[3] === player) {
-  //     legal = true;
-  //     pushChips(plane, 3, 'pos', row, col, player);
-  //   }
-  //   if (prev[0] === enemy && prev[1] === enemy && prev[2] === enemy && prev[3] === player) {
-  //     legal = true;
-  //     pushChips(plane, 3, 'neg', row, col, player);
-  //   }
-  //   if (next[0] === enemy && next[1] === enemy && next[2] === enemy && next[3] === enemy && next[4] === player) {
-  //     legal = true;
-  //     pushChips(plane, 4, 'pos', row, col, player);
-  //   }
-  //   if (prev[0] === enemy && prev[1] === enemy && prev[2] === enemy && prev[3] === enemy && prev[4] === player) {
-  //     legal = true;
-  //     pushChips(plane, 4, 'neg', row, col, player);
-  //   }
-  //   if (next[0] === enemy && next[1] === enemy && next[2] === enemy && next[3] === enemy && next[4] === enemy && next[5] === player) {
-  //     legal = true;
-  //     pushChips(plane, 5, 'pos', row, col, player);
-  //   }
-  //   if (prev[0] === enemy && prev[1] === enemy && prev[2] === enemy && prev[3] === enemy && prev[4] === enemy && prev[5] === player) {
-  //     legal = true;
-  //     pushChips(plane, 5, 'neg', row, col, player);
-  //   }
-  //   if (next[0] === enemy && next[1] === enemy && next[2] === enemy && next[3] === enemy && next[4] === enemy && next[5] === enemy && next[6] === player) {
-  //     legal = true;
-  //     pushChips(plane, 6, 'pos', row, col, player);
-  //   }
-  //   if (prev[0] === enemy && prev[1] === enemy && prev[2] === enemy && prev[3] === enemy && prev[4] === enemy && prev[5] === enemy && prev[6] === player) {
-  //     legal = true;
-  //     pushChips(plane, 6, 'neg', row, col, player);
-  //   }
-  // } // end of checkBoard function
-
   createBoard();
-  // init(confirm('Yes for 2 player, no to play computer'));
 });
